a18cd912e434a26edcbec7d65a9d73b0f7203999
jinsuc28 <jinsuc28@gmail.com>
2022-05-30 17:13:02 +0900

    집합과맵

--- 집합과맵/서다른부분문자열의개수.py
+++ 집합과맵/서다른부분문자열의개수.py
@@ -1,15 +1,30 @@
 import sys
 
-S = sys.stdin.readline()
+# 개행문자 제거 strip
+S = sys.stdin.readline().strip()
 s = []
 
-# def recursive(S):
-#     for i in range(len(S)):
+start = 0
+end = 0
 
-for i in S:
-    s.append(i)
-    for a in range(len(S)-1):
-        s.append(i+S[-a])
-        for b in range(len(S)-2)
+while True:
+    
+    # S[0:0] ''가 리스트에 들어가는 것 방지
+    if start == end:
+        s.append(S[start])
+        end +=1
+        
+    else:
+        s.append(S[start:end+1])
+        end +=1
 
-while len(S) 
+    if end == len(S):
+        start +=1
+        end = start
+        # start가 끝자리 오면 중지
+        if start == len(S):
+            break
+
+# set으로 중복제거 다시 리스트로 변환 하여 길이 반환
+s = list(set(s))
+print(len(s))
